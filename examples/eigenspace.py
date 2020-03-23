3b0943ca4660efb73be7e57cf0ab13f1262ee652
sakex <alexandre@senges.ch>
2020-03-23 21:12:32 +0100

    Back to GPy

--- examples/eigenspace.py
+++ examples/eigenspace.py
@@ -1,7 +1,5 @@
-import scipy
 import numpy as np
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import ConstantKernel, RBF, WhiteKernel
+import GPy
 
 
 DEBUG = True  # Uncomment this line for debug purposes
@@ -17,26 +15,25 @@ import sequential_learning as sl
 def c_consistence():
     n = 400
     n_var = 2
-    noise = .16
-
-    real_c = np.full((n_var, n_var), 1 / 8 * (3 + 2 * np.cos(2) - np.cos(4)))
-
-    kernel = ConstantKernel(1e-8) * RBF(length_scale=np.array([1.0, 1.0])) + WhiteKernel(noise_level=1)
-    # kernel = ConstantKernel() + Matern(length_scale=2, nu=3/2) + WhiteKernel(noise_level=1)
-    gpr = GaussianProcessRegressor(kernel=kernel, alpha=1e-10)
 
     design = np.random.uniform(size=n * n_var).reshape((-1, n_var))
 
     response = np.array([np.sin(np.sum(row)) for row in design])
-    gpr.fit(design, response)
 
-    L_inv = scipy.linalg.solve_triangular(gpr.L_.T, np.eye(gpr.L_.shape[0]))
-    k_inv = L_inv.dot(L_inv.T)
-    sigma_f = gpr.kernel_.k1.get_params()['k1__constant_value']
+    real_c = np.full((n_var, n_var), 1 / 8 * (3 + 2 * np.cos(2) - np.cos(4)))
 
-    theta = gpr.kernel_.k1.get_params()['k2__length_scale']
+    kernel = GPy.kern.Bias(n_var) * GPy.kern.RBF(n_var, lengthscale=np.array([1.0, 1.0]), ARD=True, name="rbf") + GPy.kern.White(n_var, variance=1)
+    model = GPy.models.GPRegression(design, response.reshape(-1, 1), kernel)
+    model.optimize_restarts(4)
+    model.optimize(messages=True)
 
-    mat = sl.C_gp(design, response, theta, k_inv*sigma_f, "RBF")
+    sigma_f = model.kern.mul.bias.variance.values[0] * model.kern.mul.rbf.variance.values[0]
+
+    K = model.kern.K(design) / sigma_f
+    k_inv = np.linalg.pinv(K)
+    theta = model.kern.mul.rbf.lengthscale.values
+
+    mat = sl.C_gp(design, response, theta, k_inv, "RBF")
     norm = np.linalg.norm(mat - real_c)
     assert norm < 1e-3
 
